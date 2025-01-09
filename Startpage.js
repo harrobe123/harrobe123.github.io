79f5137b63e632d6dcdab47e44e1d69c5c95dcc2
harrobe123 <74988522+harrobe123@users.noreply.github.com>
2025-01-09 08:09:26 +0000

    test

--- Startpage.js
+++ Startpage.js
@@ -4,3 +4,10 @@ function signup() {
 function login() {
   location.href = "Login.html";
 }
+const dropdownBtn = document.querySelector(".dropdown-btn");
+const dropdownMenu = document.querySelector(".dropdown-menu");
+const dropdown = document.querySelector(".dropdown");
+
+dropdownBtn.addEventListener("click", () => {
+  dropdown.classList.toggle("active");
+});
